1ad7a3a16d7536134d68d98be3dbf045bd3fcc31
milan12github <180009162+milan12github@users.noreply.github.com>
2026-03-02 22:32:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+  display.value += input;
+}
+
+function clearDisplay(){
+  display.value = "";
+}
+
+function appendToClear(e){
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate(){
+  try{
+    display.value = eval(display.value);
+  }
+  catch(error){
+    display.value = "Error";
+  }
+}
+
+
+
